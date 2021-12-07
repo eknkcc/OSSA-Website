02c3ef25fdc04b1d5828d5a9a257cc7e52e5ca9e
eknkcc <ekn573@hotmail.com>
2021-12-07 10:32:37 +0300

    Initial commit

---
+++ OSSA-Website/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OSSA_Website.Controllers
+{
+    public class HomeController : Controller
+    {
+ 
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Policy()         
+        {
+            return View();
+        }
+
+    
+    }
+}
